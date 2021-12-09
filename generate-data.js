7f804f97a7fa4a681a456fae237c1e4b3f099b9c
RioRinAkamura <nguyenthanhrin26@gmail.com>
2021-12-09 14:00:27 +0700

    change thumnailUrl

--- generate-data.js
+++ generate-data.js
@@ -40,8 +40,8 @@ const randomProductList = (categoryList, numberOfProducts) => {
                 description: faker.commerce.productDescription(),
                 createdAt: Date.now(),
                 updatedAt: Date.now(),
-                // thumnailUrl: faker.image.imageUrl(300, 300)
-                thumnailUrl: faker.image.food(300, 300)
+                thumnailUrl: faker.image.imageUrl(300, 300)
+
 
             }
 
